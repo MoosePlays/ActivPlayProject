5230386bc7aaf0bcc59c5fcb6a641a61e6fdc983
matt <mattsimonson13@yahoo.com>
2019-11-27 19:08:05 -0700

    group

--- app/src/main/java/com/example/activplay/hostActivity.java
+++ app/src/main/java/com/example/activplay/hostActivity.java
@@ -40,7 +40,6 @@ public class hostActivity extends AppCompatActivity {
         setContentView(R.layout.activity_host);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
